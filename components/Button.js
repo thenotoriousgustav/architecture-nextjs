1811f0dc9d60deac3eb06ba60471a18ddd73ffc6
thenotoriousgustav <guscae4@gmail.com>
2022-03-28 22:45:17 +0700

    button contact hidden

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 export default function Button({ href, className, variant, children, pill }) {
   const variants = {
     'outline-yellow':
-      'border border-yellow-500 text-yellow-500 hover:text-black hover:bg-yellow-500 2xl:mr-10 md:block hidden',
+      'border border-yellow-500 text-yellow-500 hover:text-black hover:bg-yellow-500 2xl:mr-10 hidden sm:inline-block',
     'yellow': 'bg-yellow-500 text-black hover:bg-yellow-600',
     'black': 'bg-black text-white hover:bg-opacity-75',
   };
